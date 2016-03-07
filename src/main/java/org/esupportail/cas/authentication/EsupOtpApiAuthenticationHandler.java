a15c159bf37f0f13c7cdcbaf7fd9172af4c37b18
Hakall <abouskine@gmail.com>
2016-03-07 10:08:04 +0100

    test verify_otp request

--- src/main/java/org/esupportail/cas/authentication/EsupOtpApiAuthenticationHandler.java
+++ src/main/java/org/esupportail/cas/authentication/EsupOtpApiAuthenticationHandler.java
@@ -24,7 +24,7 @@ public class EsupOtpApiAuthenticationHandler extends AbstractUsernamePasswordAut
 	@Override
 	protected final HandlerResult authenticateUsernamePasswordInternal(final UsernamePasswordCredential credential)throws GeneralSecurityException, PreventedException{
 		try{
-			// System.out.println(testGetRequest());
+			System.out.println(verifyOtp(credential.getUsername(), credential.getPassword()));
 			throw new FailedLoginException();
 		}catch(IOException e){
 			// System.out.println(e.toString());
@@ -34,6 +34,32 @@ public class EsupOtpApiAuthenticationHandler extends AbstractUsernamePasswordAut
 
 	private String testGetRequest() throws IOException {
 			String url = "http://localhost:3000/send_code/google_authenticator/app/john";
+
+			URL obj = new URL(url);
+			int responseCode;
+			HttpURLConnection con;
+			con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			responseCode = con.getResponseCode();
+			
+			// System.out.println("\nSending 'GET' request to URL : " + url);
+			// System.out.println("Response Code : " + responseCode);
+
+			BufferedReader in = new BufferedReader(
+				new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			return response.toString();
+	}
+
+	private String verifyOtp(String uid, String otp) throws IOException {
+			String url = "http://localhost:3000/verify_code/google_authenticator/"+uid+"/"+otp;
 
 			URL obj = new URL(url);
 			int responseCode;
