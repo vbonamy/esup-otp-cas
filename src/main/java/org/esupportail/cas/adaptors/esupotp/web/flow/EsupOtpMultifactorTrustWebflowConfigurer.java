7ac3ff725138031b541d29df6781469c0bf646fd
Vincent Bonamy <vbonamy@gmail.com>
2021-04-15 18:32:13 +0200

    en cours

--- src/main/java/org/esupportail/cas/adaptors/esupotp/web/flow/EsupOtpMultifactorTrustWebflowConfigurer.java
+++ src/main/java/org/esupportail/cas/adaptors/esupotp/web/flow/EsupOtpMultifactorTrustWebflowConfigurer.java
@@ -1,16 +1,14 @@
 package org.esupportail.cas.adaptors.esupotp.web.flow;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.web.flow.CasWebflowConstants;
-import org.apereo.cas.web.flow.configurer.AbstractMultifactorTrustedDeviceWebflowConfigurer;
-import org.springframework.context.ApplicationContext;
+import org.apereo.cas.trusted.web.flow.AbstractMultifactorTrustedDeviceWebflowConfigurer;
+import org.apereo.cas.web.flow.configurer.CasMultifactorWebflowCustomizer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
-import org.springframework.webflow.engine.Flow;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
-
-import lombok.val;
 
 
 public class EsupOtpMultifactorTrustWebflowConfigurer extends AbstractMultifactorTrustedDeviceWebflowConfigurer {
@@ -24,15 +22,17 @@ public class EsupOtpMultifactorTrustWebflowConfigurer extends AbstractMultifacto
                                                       final boolean enableDeviceRegistration,
                                                       final boolean isDeviceRegistrationRequired,
                                                       final FlowDefinitionRegistry flowDefinitionRegistry,
-                                                      final ApplicationContext applicationContext,
-                                                      final CasConfigurationProperties casProperties) {
-        super(flowBuilderServices, loginFlowDefinitionRegistry, enableDeviceRegistration && isDeviceRegistrationRequired, applicationContext, casProperties);
+                                                      final ConfigurableApplicationContext applicationContext,
+                                                      final CasConfigurationProperties casProperties,
+                                                      final List<CasMultifactorWebflowCustomizer> mfaFlowCustomizers) {
+        super(flowBuilderServices, loginFlowDefinitionRegistry, applicationContext, casProperties, Optional.of(flowDefinitionRegistry), mfaFlowCustomizers);
         this.flowDefinitionRegistry = flowDefinitionRegistry;
         this.isDeviceRegistrationRequired = isDeviceRegistrationRequired;
     }
 
     @Override
     protected void doInitialize() {
+    	/* no more needed in 6.3 ?
         val flowId = Arrays.stream(flowDefinitionRegistry.getFlowDefinitionIds()).findFirst().get();
         val flow = (Flow) flowDefinitionRegistry.getFlowDefinition(flowId);
         // Hack : override DECISION_STATE_REQUIRE_REGISTRATION that is used (and normally created) by AbstractMultifactorTrustedDeviceWebflowConfigurer.registerMultifactorTrustedAuthentication
@@ -40,6 +40,8 @@ public class EsupOtpMultifactorTrustWebflowConfigurer extends AbstractMultifacto
         createDecisionState(flow, CasWebflowConstants.DECISION_STATE_REQUIRE_REGISTRATION,
                 isDeviceRegistrationRequired.toString() + " and flashScope.".concat(MFA_TRUSTED_AUTHN_SCOPE_ATTR).concat("== null"),
                 CasWebflowConstants.VIEW_ID_REGISTER_DEVICE, CasWebflowConstants.STATE_ID_REGISTER_TRUSTED_DEVICE);
+        */
+        
         registerMultifactorTrustedAuthentication(this.flowDefinitionRegistry);
 
     }
--- src/main/java/org/esupportail/cas/config/EsupOtpConfiguration.java
+++ src/main/java/org/esupportail/cas/config/EsupOtpConfiguration.java
@@ -1,24 +1,34 @@
 package org.esupportail.cas.config;
 
 import org.apereo.cas.CentralAuthenticationService;
+import org.apereo.cas.audit.AuditableExecution;
+import org.apereo.cas.authentication.AuthenticationEventExecutionPlan;
 import org.apereo.cas.authentication.AuthenticationServiceSelectionPlan;
 import org.apereo.cas.authentication.AuthenticationSystemSupport;
+import org.apereo.cas.authentication.MultifactorAuthenticationContextValidator;
 import org.apereo.cas.authentication.MultifactorAuthenticationProviderSelector;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.ticket.TicketFactory;
+import org.apereo.cas.ticket.registry.TicketRegistry;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
 import org.apereo.cas.trusted.authentication.api.MultifactorAuthenticationTrustStorage;
+import org.apereo.cas.web.cookie.CasCookieBuilder;
 import org.apereo.cas.web.flow.CasWebflowConfigurer;
 import org.apereo.cas.web.flow.CasWebflowExecutionPlan;
 import org.apereo.cas.web.flow.CasWebflowExecutionPlanConfigurer;
+import org.apereo.cas.web.flow.SingleSignOnParticipationStrategy;
 import org.apereo.cas.web.flow.authentication.RankedMultifactorAuthenticationProviderSelector;
+import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
+import org.apereo.cas.web.flow.resolver.impl.CasWebflowEventResolutionConfigurationContext;
 import org.esupportail.cas.adaptors.esupotp.EsupOtpService;
 import org.esupportail.cas.adaptors.esupotp.web.flow.EsupOtpAuthenticationWebflowAction;
 import org.esupportail.cas.adaptors.esupotp.web.flow.EsupOtpAuthenticationWebflowEventResolver;
 import org.esupportail.cas.adaptors.esupotp.web.flow.EsupOtpGetTransportsAction;
 import org.esupportail.cas.adaptors.esupotp.web.flow.EsupOtpMultifactorTrustWebflowConfigurer;
 import org.esupportail.cas.adaptors.esupotp.web.flow.EsupOtpMultifactorWebflowConfigurer;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -31,7 +41,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.web.util.CookieGenerator;
 import org.springframework.webflow.config.FlowDefinitionRegistryBuilder;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
@@ -49,6 +58,14 @@ public class EsupOtpConfiguration {
 
 	@Autowired
 	private ConfigurableApplicationContext applicationContext;
+	
+    @Autowired
+    @Qualifier("defaultTicketFactory")
+    private ObjectProvider<TicketFactory> ticketFactory;
+
+    @Autowired
+    @Qualifier("authenticationEventExecutionPlan")
+    private ObjectProvider<AuthenticationEventExecutionPlan> authenticationEventExecutionPlan;
 
 	@Autowired
 	@Qualifier("loginFlowRegistry")
@@ -77,13 +94,34 @@ public class EsupOtpConfiguration {
 	@Qualifier("servicesManager")
 	private ServicesManager servicesManager;
 
-	@Autowired
-	@Qualifier("warnCookieGenerator")
-	private CookieGenerator warnCookieGenerator;
+    @Autowired
+    @Qualifier("singleSignOnParticipationStrategy")
+    private ObjectProvider<SingleSignOnParticipationStrategy> webflowSingleSignOnParticipationStrategy;
+
+    @Autowired
+    @Qualifier("registeredServiceAccessStrategyEnforcer")
+    private ObjectProvider<AuditableExecution> registeredServiceAccessStrategyEnforcer;
+
 
     @Autowired
     @Qualifier("authenticationServiceSelectionPlan")
-    private AuthenticationServiceSelectionPlan authenticationRequestServiceSelectionStrategies;
+    private ObjectProvider<AuthenticationServiceSelectionPlan> authenticationRequestServiceSelectionStrategies;
+
+    @Autowired
+    @Qualifier("initialAuthenticationAttemptWebflowEventResolver")
+    private ObjectProvider<CasDelegatingWebflowEventResolver> initialAuthenticationAttemptWebflowEventResolver;
+
+    @Autowired
+    @Qualifier("authenticationContextValidator")
+    private ObjectProvider<MultifactorAuthenticationContextValidator> authenticationContextValidator;
+
+    @Autowired
+    @Qualifier("warnCookieGenerator")
+    private ObjectProvider<CasCookieBuilder> warnCookieGenerator;
+
+    @Autowired
+    @Qualifier("ticketRegistry")
+    private ObjectProvider<TicketRegistry> ticketRegistry;
     
     @Autowired
     private ApplicationEventPublisher eventPublisher;
@@ -120,16 +158,25 @@ public class EsupOtpConfiguration {
 
 	@Bean
 	public CasWebflowEventResolver esupotpAuthenticationWebflowEventResolver() {
-		return new EsupOtpAuthenticationWebflowEventResolver(
-			authenticationSystemSupport, 
-            centralAuthenticationService, 
-            servicesManager, 
-            ticketRegistrySupport,
-            warnCookieGenerator, 
-            authenticationRequestServiceSelectionStrategies, 
-            eventPublisher, 
-            applicationContext
-		);
+		
+		CasWebflowEventResolutionConfigurationContext context = CasWebflowEventResolutionConfigurationContext.builder()
+		            .casDelegatingWebflowEventResolver(initialAuthenticationAttemptWebflowEventResolver.getObject())
+		            .authenticationContextValidator(authenticationContextValidator.getObject())
+		            .authenticationSystemSupport(authenticationSystemSupport)
+		            .centralAuthenticationService(centralAuthenticationService)
+		            .servicesManager(servicesManager)
+		            .ticketRegistrySupport(ticketRegistrySupport)
+		            .warnCookieGenerator(warnCookieGenerator.getObject())
+		            .authenticationRequestServiceSelectionStrategies(authenticationRequestServiceSelectionStrategies.getObject())
+		            .registeredServiceAccessStrategyEnforcer(registeredServiceAccessStrategyEnforcer.getObject())
+		            .casProperties(casProperties)
+		            .singleSignOnParticipationStrategy(webflowSingleSignOnParticipationStrategy.getObject())
+		            .ticketRegistry(ticketRegistry.getObject())
+		            .applicationContext(applicationContext)
+		            .authenticationEventExecutionPlan(authenticationEventExecutionPlan.getObject())
+		            .build();
+		 
+		return new EsupOtpAuthenticationWebflowEventResolver(context);
 	}
     
     @ConditionalOnMissingBean(name = "esupotpMultifactorWebflowConfigurer")
